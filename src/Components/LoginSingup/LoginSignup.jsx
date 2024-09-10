035643a94ab70d2c8ec60e55e48b311cb7c79e07
Himanshu Bhandari <himansh8755529677@gmail.com>
2024-09-10 10:31:38 +0530

    add validation on input field

--- src/Components/LoginSingup/LoginSignup.jsx
+++ src/Components/LoginSingup/LoginSignup.jsx
@@ -5,9 +5,30 @@ import pwd_icon from '../Assets/password.png'
 import './LoginSignup.css'
 
 const LoginSignup = () => {
+  const [user,setUser]=useState({});
   const [action ,setAction]=useState("Login");
 
+  function handleUser(e){
+    setUser({...user,[e.target.name]:e.target.value});
+  }
 
+  function submitSignupForm(){
+    console.log(user);
+    validateUser();
+  }
+
+  function validateUser(){
+    if(action==="Sign Up" && !user.name && !user.email  && !user.password && !user.confirmPassword){
+      alert("Please enter valid field");
+    }else if(action==="Sign Up" && user.password!==user.confirmPassword){
+      alert("password and confirm password not match");
+    }else if(!user.email  && !user.password){
+      alert("please fill all field");
+    }
+    else{
+      alert("form fill successfully");
+    }
+  }
   return (
     <div className='container'>
         <div className="header">
@@ -18,23 +39,23 @@ const LoginSignup = () => {
           {action==="Login"?<div></div>:
             <div className="input">
               <img src={user_icon} alt="user" />
-              <input type="text" placeholder='Name'/>
+              <input type="text" name="name" placeholder='Name' onChange={(e)=>{handleUser(e)}}/>
             </div>
           }
             <div className="input">
               <img src={email_icon} alt="email" />
-              <input type="email" placeholder='email'/>
+              <input type="email" name="email" placeholder='email' onChange={(e)=>{handleUser(e)}}/>
             </div>
 
             <div className="input">
               <img src={pwd_icon} alt="password" />
-              <input type="password" placeholder='password'/>
+              <input type="password" name="password" placeholder='password' onChange={(e)=>{handleUser(e)}}/>
             </div>
             
             {action==="Login"?<div></div>:
               <div className="input">
                 <img src={pwd_icon} alt="password" />
-                <input type="password" placeholder='Confirm password'/>
+                <input type="password" name="confirmPassword" placeholder='Confirm password' onChange={(e)=>{handleUser(e)}}/>
               </div>
             }
         </div>
@@ -45,6 +66,7 @@ const LoginSignup = () => {
           <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
           <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
         </div>
+        <button onClick={(e)=>{submitSignupForm(e)}}>Click Me</button>
     </div>
   )
 }
